f846ae722a7deea66c15b2da2c716e0c6e8a7038
“juwanpetty” <jchpetty@gmail.com>
2022-02-02 21:12:14 -0500

    Add tab bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import 'styles/main.scss';
+import 'styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,45 @@
-import Home from 'views/Home';
+import { Tab } from '@headlessui/react';
+import {
+  AdjustmentsIcon,
+  ClockIcon,
+  CollectionIcon
+} from '@heroicons/react/outline';
 
 export default function Index() {
-  return <Home />;
+  return (
+    <Tab.Group as="div" className="w-80 rounded-lg bg-white shadow-xl">
+      <Tab.Panels>
+        <Tab.Panel>Content 1</Tab.Panel>
+        <Tab.Panel>Content 2</Tab.Panel>
+        <Tab.Panel>Content 3</Tab.Panel>
+      </Tab.Panels>
+
+      <Tab.List className="flex h-14 items-center justify-around border-t border-neutral-200">
+        <Tab
+          className={({ selected }) =>
+            selected ? 'text-blue-500' : 'text-gray-700'
+          }>
+          <div className="flex items-center">
+            <ClockIcon className="h-5 w-5" aria-hidden="true" />
+          </div>
+        </Tab>
+        <Tab
+          className={({ selected }) =>
+            selected ? 'text-blue-500' : 'text-gray-700'
+          }>
+          <div className="flex items-center">
+            <CollectionIcon className="h-5 w-5 " aria-hidden="true" />
+          </div>
+        </Tab>
+        <Tab
+          className={({ selected }) =>
+            selected ? 'text-blue-500' : 'text-gray-700'
+          }>
+          <div className="flex items-center">
+            <AdjustmentsIcon className="h-5 w-5 " aria-hidden="true" />
+          </div>
+        </Tab>
+      </Tab.List>
+    </Tab.Group>
+  );
 }
